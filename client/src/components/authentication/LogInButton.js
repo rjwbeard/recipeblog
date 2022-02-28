cbc15550146e4eee8a3e626912706648ae4c3ac3
Rebecca Beard <rbccbeard@gmail.com>
2022-02-28 11:12:14 -0500

    TESTING: loginWithPopup

--- client/src/components/authentication/LogInButton.js
+++ client/src/components/authentication/LogInButton.js
@@ -1,14 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogInButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
 
   return (
     <button
       className="Authentication_buttons"
       onClick={(e) => {
         console.log(e);
-        loginWithRedirect();
+        loginWithPopup();
       }}
     >
       Log In
